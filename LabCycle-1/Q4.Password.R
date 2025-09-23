15a6405899785b4ec9c06607ebcac1d917c37954
snehal9066 <147644050+snehal9066@users.noreply.github.com>
2025-09-23 22:53:19 +0530

    Update Q4.Password.R

--- LabCycle-1/Q4.Password.R
+++ LabCycle-1/Q4.Password.R
@@ -1,28 +1,19 @@
-password <- function() {
-  len <- as.integer(readline(prompt = "Enter the length of password: "))
-
-  # Validate password length
-  while (is.na(len) || len < 6) {
-    cat("Please enter a valid length (at least 6):\n")
-    len <- as.integer(readline(prompt = "Enter length: "))
-  }
-
-  # Character sets
-  upper <- LETTERS
-  lower <- letters
-  digits <- as.character(0:9)
-  special <- c('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '~')
-
-  # Combine all characters
-  full <- c(upper, lower, digits, special)
-
-  # Generate password
-  pass <- sample(full, len, replace = TRUE)
-  pass <- paste0(pass, collapse = "")
-
-  # Print password
-  cat(sprintf("The generated password is: %s\n", pass))
+generate_password<-function(len){
+    password<-""
+    all_chars<-intToUtf8(33:126,multiple=TRUE)
+    uppercase<-LETTERS
+    lowercase<-letters
+    numbers<-as.character(0:9)
+    special<-setdiff(all_chars,c(uppercase,lowercase,numbers))
+    mandatory<-c(sample(uppercase,1),sample(lowercase,1),sample(numbers,1),sample(special,1))
+    remaining<-c(sample(c(uppercase,lowercase,numbers,special),len-length(mandatory)))
+    password<-paste0(password,sample(c(mandatory,remaining)),collapse="")
+    return(password)
 }
-
-# Run the function
-password()
+len<-as.numeric(readline(prompt="Enter the length of the password:"))
+if(len>=8){
+    pass<-generate_password(len)
+    cat("Generated password:",pass)
+}else{
+    cat("Please enter a valid length")
+}
